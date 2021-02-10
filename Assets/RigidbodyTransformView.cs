55a9cca6870ebd9c7457f837ac58a79fdd2b3ba8
BadAlchemy <36778218+BadAlchemy@users.noreply.github.com>
2021-02-10 15:21:43 -0600

    Sync rot

--- Assets/RigidbodyTransformView.cs
+++ Assets/RigidbodyTransformView.cs
@@ -10,6 +10,8 @@ public class RigidbodyTransformView : MonoBehaviour, IPunObservable {
     Quaternion latestRot;
     Vector3 velocity;
     Vector3 angularVelocity;
+    Quaternion latestPropRot;
+    GameObject propHolder;
     [SerializeField]
     float lerpSpeed;
 
@@ -25,16 +27,28 @@ public class RigidbodyTransformView : MonoBehaviour, IPunObservable {
         rb = GetComponent<Rigidbody>();
     }
 
+    void ResetPropHolder() {
+        propHolder = transform.Find("PropHolder").gameObject;
+    }
+
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) {
         if (stream.IsWriting) {
             //We own this player: send the others our data
             if (rb == null) {
                 ResetRB();
+            } else if (propHolder == null) {
+                ResetPropHolder();
             }
+            //Player
             stream.SendNext(transform.position);
             stream.SendNext(transform.rotation);
             stream.SendNext(rb.velocity);
             stream.SendNext(rb.angularVelocity);
+            //Child Prop
+            if (propHolder.transform.childCount > 0) {
+                stream.SendNext(propHolder.transform.GetChild(0).rotation);
+            }
+
 
         } else {
             //Network player, receive data
@@ -42,6 +56,9 @@ public class RigidbodyTransformView : MonoBehaviour, IPunObservable {
             latestRot = (Quaternion)stream.ReceiveNext();
             velocity = (Vector3)stream.ReceiveNext();
             angularVelocity = (Vector3)stream.ReceiveNext();
+            //Child Prop
+            latestPropRot = (Quaternion)stream.ReceiveNext();
+
 
             valuesReceived = true;
 
@@ -53,16 +70,25 @@ public class RigidbodyTransformView : MonoBehaviour, IPunObservable {
     {
         if (!pv.IsMine && valuesReceived) {
             //Update Object position and Rigidbody parameters
+            //Player
             if (rb == null) {
                 ResetRB();
+            } else if (propHolder == null) {
+                ResetPropHolder();
             }
-
-            Debug.Log("Sending data:   " + latestPos + "   "  + latestRot + "   " + velocity + "   " + angularVelocity);
-
             transform.position = Vector3.Lerp(transform.position, latestPos, Time.deltaTime * lerpSpeed);
             transform.rotation = Quaternion.Lerp(transform.rotation, latestRot, Time.deltaTime * lerpSpeed);
-            rb.velocity = Vector3.Lerp(rb.velocity, velocity, Time.deltaTime * lerpSpeed);
-            rb.angularVelocity = Vector3.Lerp(rb.angularVelocity, angularVelocity, Time.deltaTime * lerpSpeed);
+            rb.velocity = velocity;
+            rb.angularVelocity = angularVelocity;
+            //Prop
+            //Child Prop
+            if (propHolder.transform.childCount > 0) {
+                propHolder.transform.GetChild(0).rotation = latestPropRot;
+            }
+
+            // Lerping player's rb values resulted in small amounts of rubber banding. This could likely be tweaked to fix if necessary.
+            //rb.velocity = Vector3.Lerp(rb.velocity, velocity, Time.deltaTime * lerpSpeed);
+            //rb.angularVelocity = Vector3.Lerp(rb.angularVelocity, angularVelocity, Time.deltaTime * lerpSpeed);
         }
     }
 }
