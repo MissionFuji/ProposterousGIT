f777e095042d59735708e472b8d868a57603c87a
BadAlchemy <36778218+BadAlchemy@users.noreply.github.com>
2021-02-22 22:01:24 -0600

    Update VoidReset.cs

--- Assets/_Assets/Scripts/Utility/VoidReset.cs
+++ Assets/_Assets/Scripts/Utility/VoidReset.cs
@@ -12,9 +12,10 @@ public class VoidReset : MonoBehaviour
 
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.GetComponent<PropInteraction>()) {
-            if (other.gameObject.transform.parent != null) { //Let's see if it has a parent. If not, it's a prop. If so, it's a player.
+            if (other.gameObject.tag == "AttachedProp") { //Let's see if it's an attached prop.
+                //We get from the prop, to the prop holder, to the player.
                 voidPV.RPC("RPC_VoidResetPosition", RpcTarget.AllBuffered, other.gameObject.transform.parent.transform.parent.gameObject.GetComponent<PhotonView>().ViewID);
-            } else {
+            } else { // Unattached prop.
                 voidPV.RPC("RPC_VoidResetPosition", RpcTarget.AllBuffered, other.gameObject.GetComponent<PhotonView>().ViewID);
             }
         }
@@ -23,7 +24,7 @@ public class VoidReset : MonoBehaviour
     [PunRPC]
     private void RPC_VoidResetPosition(int viewID) {
         PhotonView fallingProp = PhotonView.Find(viewID);
-        Debug.LogWarning(fallingProp.gameObject.name + " has fallen off of the map. Reseting position over network.");
+        Debug.LogWarning(fallingProp.gameObject.name + " has fallen off of the map. Resetting position over network.");
         fallingProp.gameObject.transform.position = new Vector3(0f,7f,0f);
     }
 
