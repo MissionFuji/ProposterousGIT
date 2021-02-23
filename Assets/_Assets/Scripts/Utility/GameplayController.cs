da7a69bbc9700a9a112c51db3e7a9d96ef2d16f0
BadAlchemy <36778218+BadAlchemy@users.noreply.github.com>
2021-02-23 17:05:16 -0600

    Update GameplayController.cs

--- Assets/_Assets/Scripts/Utility/GameplayController.cs
+++ Assets/_Assets/Scripts/Utility/GameplayController.cs
@@ -164,6 +164,7 @@ public class GameplayController : MonoBehaviour
                 //We're a seeker.
                 PhotonView ourPV = PhotonView.Find(myID);
                 ourPV.GetComponent<Rigidbody>().isKinematic = true; // Freeze our player, we will unfreeze after prop is spawned, and modified through callback in PropInteraction.
+                ourPV.gameObject.transform.rotation = Quaternion.identity;
                 PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "Player_Seeker"), ourPV.gameObject.transform.position, Quaternion.identity, 0, instanceData); //Spawn our ghost prop.
             }
         }
@@ -173,6 +174,7 @@ public class GameplayController : MonoBehaviour
                 //We're a pre-prop ghost.
                 PhotonView ourPV = PhotonView.Find(myID);
                 ourPV.GetComponent<Rigidbody>().isKinematic = true; // Freeze our player, we will unfreeze after prop is spawned, and modified through callback in PropInteraction.
+                ourPV.gameObject.transform.rotation = Quaternion.identity;
                 PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "Player_Ghost"), ourPV.gameObject.transform.position, Quaternion.identity, 0, instanceData); //Spawn our ghost prop.
             }
         }
