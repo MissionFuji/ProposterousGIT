30c8898c11f68b43fd81dfd72a724ddd33b3c03e
BadAlchemy <36778218+BadAlchemy@users.noreply.github.com>
2021-02-24 15:15:08 -0600

    Update NameTagHolder.cs

--- Assets/_Assets/Scripts/Player/NameTagHolder.cs
+++ Assets/_Assets/Scripts/Player/NameTagHolder.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 
 public class NameTagHolder : MonoBehaviourPunCallbacks, IInRoomCallbacks {
     public int ownerID = -1;
+    private int actorID = -1;
     public GameObject tarPlayer = null;
 
     // Update is called once per frame
@@ -14,8 +15,15 @@ public class NameTagHolder : MonoBehaviourPunCallbacks, IInRoomCallbacks {
         if (tarPlayer != null) {
             if (ownerID == -1) {
                 ownerID = tarPlayer.GetComponent<PhotonView>().ViewID;
+                actorID = tarPlayer.GetComponent<PhotonView>().Owner.ActorNumber;
             }
             gameObject.transform.position = tarPlayer.transform.position + new Vector3(0, tarPlayer.transform.localScale.y + 1f, 0);
+        } else {
+            if (!PhotonNetwork.CurrentRoom.Players.ContainsKey(actorID)) {
+                Destroy(gameObject);
+            } else {
+                Debug.LogError("Player is null but is still found in the game via Actor#?");
+            }
         }
     }
 }
